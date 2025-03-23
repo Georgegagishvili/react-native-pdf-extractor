42f827ecaa1293c01a30cab606ecb56c58af7880
georgegagishvili <georgegagishvili@gmail.com>
2025-03-23 18:31:49 +0400

    fix: imports

--- android/src/main/java/com/pdfextractor/PdfExtractorModule.kt
+++ android/src/main/java/com/pdfextractor/PdfExtractorModule.kt
@@ -1,5 +1,7 @@
 package com.pdfextractor
 
+import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
