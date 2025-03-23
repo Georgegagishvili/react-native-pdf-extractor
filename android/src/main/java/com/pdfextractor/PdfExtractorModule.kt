ded588ac50885269cec02562e0177ba6940ff480
georgegagishvili <georgegagishvili@gmail.com>
2025-03-23 18:30:20 +0400

    fix: types

--- android/src/main/java/com/pdfextractor/PdfExtractorModule.kt
+++ android/src/main/java/com/pdfextractor/PdfExtractorModule.kt
@@ -1,17 +1,11 @@
 package com.pdfextractor
 
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.annotations.ReactModule
-import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Callback
-
-import com.tom_roush.pdfbox.pdmodel.PDDocument;
-import com.tom_roush.pdfbox.text.PDFTextStripper;
-import com.tom_roush.pdfbox.android.PDFBoxResourceLoader;
-
-import android.content.Context
-
+import com.facebook.react.bridge.Promise
+import com.tom_roush.pdfbox.pdmodel.PDDocument
+import com.tom_roush.pdfbox.text.PDFTextStripper
+import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 import java.io.File
 import java.io.FileInputStream
 
@@ -24,7 +18,7 @@ class PdfExtractorModule(reactContext: ReactApplicationContext) :
   }
 
   @ReactMethod
-  override fun extractTextFromPdf(filePath: String, callback: Callback) {
+  override fun extractTextFromPdf(filePath: String, promise: Promise) {
     Thread {
         try {
             val file = File(filePath)
@@ -33,9 +27,9 @@ class PdfExtractorModule(reactContext: ReactApplicationContext) :
             val stripper = PDFTextStripper()
             val text = stripper.getText(document)
             document.close()
-            callback.invoke(null, text)
+            promise.resolve(text)
         } catch (e: Exception) {
-            callback.invoke(e.message, null)
+            promise.reject("PdfExtractorError", e)
         }
     }.start()
   }
